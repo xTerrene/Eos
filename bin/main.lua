9eeddaad097bcf584543ed9af71ef5284ef780fa
EarthToAccess <oldmonkey333@gmail.com>
2023-01-19 14:48:37 -0500

    deprecated

--- bin/main.lua
+++ bin/main.lua
@@ -8,3 +8,6 @@ end, function(e)
     print('encountered error. xpcall says:', e)
     warn('traceback: ',debug.traceback())
 end)
+
+warn("ATTN: This version of EBGui is no longer being maintained.")
+print("Please load from https://raw.githubusercontent.com/EarthToAccess/EBGUI/main/EBGui.lua to get updates.")
